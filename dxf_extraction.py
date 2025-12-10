d4cc305a4a6ce64358a9968665f4dc21534f5482
sismo68 <35981425+sismo68@users.noreply.github.com>
2025-12-10 14:44:52 -0500

    Update dxf_extraction.py

--- dxf_extraction.py
+++ dxf_extraction.py
@@ -458,7 +458,7 @@ def process_dxf(dxf_file_path, selected_layer, z_offset, output_dxf_name):
                 'insert': (x_coord, y_coord, z_coord),  # Specify position directly here
     #            'set_elevation' : 'MIDDLE',
     #            'halign' : 4,
-    #            'rgb': ed.colors.RGB(colorRGB[0], colorRGB[1], colorRGB[2]) # Uncomment if you want to use the color from DataFrame
+                 'rgb': ed.colors.RGB(colorRGB[0], colorRGB[1], colorRGB[2]) # Uncomment if you want to use the color from DataFrame
                 # Or use: 'true_color': text.dxf.true_color  # If this is what you intended
             }, 
         )     # .set_align_enum(align=text_content.Alignment.LEFT)
@@ -494,3 +494,4 @@ def process_dxf(dxf_file_path, selected_layer, z_offset, output_dxf_name):
     
 
     return mtext_df , chairs_df
+
